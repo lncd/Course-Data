10cf88fbf8e7733c9de4d818029d51264a5f4b3b
jmahoney127 <jmahoney@cerds-imac.local>
2012-03-02 16:01:46 +0000

    I prevented a massive project meltdown by placing a space after a comma
    HERO

--- src/application/models/dummy_data_model.php
+++ src/application/models/dummy_data_model.php
@@ -760,7 +760,7 @@ class Dummy_data_model extends CI_Model {
 		$password = 'root';
 		$hostname = 'localhost';	
 		$dbh = mysql_connect($hostname, $username, $password) or die("Unable to connect to MySQL");
-		mysql_select_db('modules',$dbh);
+		mysql_select_db('modules', $dbh);
 		
 		$sql = "SELECT * FROM 13_programmes WHERE programme = '" . $course['ucasProgrammeCode'] . "'";
 		$results = mysql_query($sql) or die('Didnt work.');
@@ -1064,7 +1064,7 @@ class Dummy_data_model extends CI_Model {
 		
 		$sqla = "SELECT * FROM 13_programmes WHERE programme = '" . $course['ucasProgrammeCode'] . "'";
 		$results = mysql_query($sqla) OR die('Didnt work.');
-		while($a_row = mysql_fetch_array($results,MYSQL_ASSOC))
+		while($a_row = mysql_fetch_array($results, MYSQL_ASSOC))
 		{
 			$course['modules'][] = $a_row['moduleCode'];
 		}
@@ -1204,11 +1204,11 @@ class Dummy_data_model extends CI_Model {
 		$password = 'root';
 		$hostname = 'localhost';	
 		$dbh = mysql_connect($hostname, $username, $password) OR die('Unable to connect to MySQL');
-		mysql_select_db('modules',$dbh);
+		mysql_select_db('modules', $dbh);
 		
 		$sql = "SELECT * FROM 13_programmes WHERE programme = '" . $course['ucasProgrammeCode'] . "'";
 		$results = mysql_query($sql) OR die('Didnt work.');
-		while($a_row = mysql_fetch_array($results,MYSQL_ASSOC))
+		while($a_row = mysql_fetch_array($results, MYSQL_ASSOC))
 		{
 			$course['modules'][] = $a_row['moduleCode'];
 		}
@@ -1322,28 +1322,28 @@ class Dummy_data_model extends CI_Model {
 			$coOrd = mysql_fetch_row(mysql_query($sql3));
 			$module['moduleCoOrdinator'] = 	$coOrd[1];
 			$module['moduleCoOrdinatorID'] = $a_row['moduleCoordinator'];
-			$module['moduleSynopsis'] = preg_replace('/[^(\x20-\x7F)]*/','', nl2br($a_row['moduleSynopsis']));
-			$module['outlineSyllabus'] = preg_replace('/[^(\x20-\x7F)]*/','', nl2br($a_row['outlineSyllabus']));
-			$module['ltMethods'] = preg_replace('/[^(\x20-\x7F)]*/','', nl2br($a_row['l_tMethods']));
+			$module['moduleSynopsis'] = preg_replace('/[^(\x20-\x7F)]*/', '',  nl2br($a_row['moduleSynopsis']));
+			$module['outlineSyllabus'] = preg_replace('/[^(\x20-\x7F)]*/', '',  nl2br($a_row['outlineSyllabus']));
+			$module['ltMethods'] = preg_replace('/[^(\x20-\x7F)]*/', '',  nl2br($a_row['l_tMethods']));
 			if($a_row['accreditation'] != '')
 				$module['accreditation'] = $a_row['accreditation'];
 			else
 				$module['accreditation'] = '';
-			$module['indicativeReading'] = preg_replace('/[^(\x20-\x7F)]*/','', nl2br($a_row['indicativeReading']));
+			$module['indicativeReading'] = preg_replace('/[^(\x20-\x7F)]*/', '',  nl2br($a_row['indicativeReading']));
 			$module['learningOutcomes'] = array();
 			
 			$sql4 = "SELECT * FROM 05_learningOutcomes INNER JOIN 06_moduleLearningOutcomes ON 06_moduleLearningOutcomes.outcomeID = 05_learningOutcomes.outcomeID WHERE 06_moduleLearningOutcomes.moduleCode = '" . $module['_id'] . "'";
 			$outcomes = mysql_query($sql4) or die('Could not execute outcomes query.');
 						
-			while($row1 = mysql_fetch_array($outcomes,MYSQL_ASSOC))
+			while($row1 = mysql_fetch_array($outcomes, MYSQL_ASSOC))
 			{
-				$module['learningOutcomes'][] = preg_replace('/[^(\x20-\x7F)]*/','',$row1['outcomeText']);
+				$module['learningOutcomes'][] = preg_replace('/[^(\x20-\x7F)]*/', '', $row1['outcomeText']);
 			}
 			
 			$sql5 = "SELECT * FROM 07_assessments INNER JOIN 09_moduleAssessments ON 09_moduleAssessments.assessmentID = 07_assessments.assessmentID WHERE 09_moduleAssessments.moduleCode = '" . $module['_id'] . "'";
 			$assessments = mysql_query($sql5) or die('Could not execute assessments query.');
 			$module['assessments'] = array();
-			while($row2 = mysql_fetch_array($assessments,MYSQL_ASSOC))
+			while($row2 = mysql_fetch_array($assessments, MYSQL_ASSOC))
 			{
 				$module['assessments'][] = array(
 												'assessmentMethod' => $row2['assessmentMethod'],
@@ -1355,7 +1355,7 @@ class Dummy_data_model extends CI_Model {
 			$sql6 = "SELECT * FROM 02_staff INNER JOIN 04_teachingDemoStaff ON 04_teachingDemoStaff.staffID = 02_staff.staffID WHERE 04_teachingDemoStaff.moduleCode = '" . $module['_id'] . "'";
 			$teaching = mysql_query($sql6) or die('Could not execute teaching staff query.');
 			$module['teachingStaff'] = array();
-			while($row3 = mysql_fetch_array($teaching,MYSQL_ASSOC))
+			while($row3 = mysql_fetch_array($teaching, MYSQL_ASSOC))
 			{
 				$module['teachingStaff'][] = $row3['staffID'];
 			}
@@ -1363,7 +1363,7 @@ class Dummy_data_model extends CI_Model {
 			$module['contactTime'] = array();
 			$sql7 = "SELECT * FROM 10_contact INNER JOIN 11_moduleContact ON 11_moduleContact.contactID = 10_contact.contactID WHERE 11_moduleContact.moduleCode = '" . $module['_id'] . "'";
 			$contacts = mysql_query($sql7) or die('Could not execute contact time query.');
-			while($row4 = mysql_fetch_array($contacts,MYSQL_ASSOC))
+			while($row4 = mysql_fetch_array($contacts, MYSQL_ASSOC))
 			{
 				$module['contactTime'][] = array(
 												'type' => $row4['type'],
@@ -1374,7 +1374,7 @@ class Dummy_data_model extends CI_Model {
 			$module['jacsCodes'] = array();
 			$sql8 = "SELECT * FROM 12_jacsCodes WHERE moduleCode = '" . $module['_id'] . "'";
 			$jacs = mysql_query($sql8) or die('Could not execute JACS code query.');
-			while($row5 = mysql_fetch_array($jacs,MYSQL_ASSOC))
+			while($row5 = mysql_fetch_array($jacs, MYSQL_ASSOC))
 			{
 				$module['jacsCodes'][] = $row5['jacsCode'];
 			}
@@ -1382,7 +1382,7 @@ class Dummy_data_model extends CI_Model {
 			$module['preRequisites'] = array();
 			$sql9 = "SELECT * FROM 14_preReq WHERE moduleCode = '" . $module['_id'] . "'";
 			$pre = mysql_query($sql9) or die('Could not execute pre requisites query.');
-			while($row5 = mysql_fetch_array($pre,MYSQL_ASSOC))
+			while($row5 = mysql_fetch_array($pre, MYSQL_ASSOC))
 			{
 				$module['preRequisites'][] = $row5['preRequisite'];
 			}
@@ -1390,7 +1390,7 @@ class Dummy_data_model extends CI_Model {
 			$module['coRequisites'] = array();
 			$sql9 = "SELECT * FROM 15_coReq WHERE moduleCode = '" . $module['_id'] . "'";
 			$pre = mysql_query($sql9) or die('Could not execute pre requisites query.');
-			while($row5 = mysql_fetch_array($pre,MYSQL_ASSOC))
+			while($row5 = mysql_fetch_array($pre, MYSQL_ASSOC))
 			{
 				$module['coRequisites'][] = $row5['coRequisite'];
 			}
@@ -1398,7 +1398,7 @@ class Dummy_data_model extends CI_Model {
 			$module['barredCombinations'] = array();
 			$sql9 = "SELECT * FROM 16_barredCombinations WHERE moduleCode1 = '" . $module['_id'] . "'";
 			$pre = mysql_query($sql9) or die('Could not execute barred combinations query.');
-			while($row5 = mysql_fetch_array($pre,MYSQL_ASSOC))
+			while($row5 = mysql_fetch_array($pre, MYSQL_ASSOC))
 			{
 				$module['barredCombinations'][] = $row5['moduleCode2'];
 			}
