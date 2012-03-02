c6b2f54ef9a1a5194c862862d12c252e6f6cb807
jmahoney127 <jmahoney@cerds-imac.local>
2012-03-02 15:13:42 +0000

    More Jenkins style issues corrected.

--- src/application/controllers/dummy_data.php
+++ src/application/controllers/dummy_data.php
@@ -1,6 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
 /**
  * MyClass File Doc Comment
  *
@@ -18,7 +16,7 @@ ini_set('display_errors', '1');
  * Dummy_Data
  *
  * @category DummyData
- * @package	 DummyData
+ * @package	DummyData
  * @author   Jamie Mahoney <jmahoney127@googlemail.com>
  * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
  * @link     coursedata.blogs.lincoln.ac.uk
@@ -46,53 +44,59 @@ class Dummy_data extends CI_Controller {
 		$this->dummy_data_model->xcri('http://localhost:8888/cdata/dummyData/xcri.xml');
 	}
 	
-	private function _test_api($filename = "strategy.txt")
+	/**
+	* Function to test api is functioning as expected
+	*
+	* @return Nothing
+	* @access Private
+	*/
+	private function _test_api($filename = 'strategy.txt')
 	{
-        $lines = array();
-        $handle = @fopen($filename, "r");
+		$lines = array();
+		$handle = @fopen($filename, 'r');
         
         if ($handle)
         {
-            while (($buffer = fgets($handle, 4096)) !== false){
-            $lines[] = explode(';',$buffer);
-            }
-            if (!feof($handle)) {
-                echo "Error: unexpected fgets() fail\n";
-            }
-            fclose($handle);
-            $results = array('amount' => sizeof($lines) - 1, 'pass' => 0, 'fail' => 0);
-            $errors = array();
-            try{
-                if((sizeof($lines[0])) == 1)
-                {
-                    $newlines = array("\t","\n","\r","\x20\x20","\0","\x0B");
-                    $urlBase = str_replace($newlines, "", html_entity_decode($lines[0][0]));
-                }
-                else
-                    throw new Exception('Line 1 did not contain the base url.');
-            
-                for($i = 1; $i < sizeof($lines); $i++)
-                {
-                    $url = $urlBase . $lines[$i][0] . '?' . $lines[$i][1];
-                    $url = str_replace(' ','',$url);
-                    $check = json_decode(file_get_contents($url));
-                
-                    if(($check->error == strval($lines[$i][2])) && ($check->count == strval($lines[$i][3])))
-                        $results['pass'] += 1;
-                    else
-                    {
-                        $results['fail'] += 1;
-                        $errors[] = 'API: ' . $lines[$i][0] . '. Criteria: ' . $lines[$i][1];
-                    }
-                }
-            }
-            catch(Exception $e)
-            {
-                echo $e;
-            }
-        
-            return array('results' => $results, 'errors' => $errors);
-        }
+        	while (($buffer = fgets($handle, 4096)) !== FALSE){
+        	$lines[] = explode(';', $buffer);
+        	}
+        	if (! feof($handle)) {
+				echo "Error: unexpected fgets() fail\n";
+			}
+			fclose($handle);
+			$results = array('amount' => sizeof($lines) - 1, 'pass' => 0, 'fail' => 0);
+			$errors = array();
+			try{
+				if((sizeof($lines[0])) === 1)
+				{
+					$newlines = array('\t','\n','\r','\x20\x20','\0','\x0B');
+					$url_base = str_replace($newlines, '', html_entity_decode($lines[0][0]));
+				}
+				else
+					throw new Exception('Line 1 did not contain the base url.');
+					
+				for($i = 1; $i < sizeof($lines); $i++)
+				{
+					$get_url = $url_base . $lines[$i][0] . '?' . $lines[$i][1];
+					$get_url = str_replace(' ', '', $url);
+					$check = json_decode(file_get_contents($get_url));
+					
+					if(($check->error === strval($lines[$i][2])) && ($check->count === strval($lines[$i][3])))
+						$results['pass'] += 1;
+					else
+					{
+						$results['fail'] += 1;
+						$errors[] = 'API: ' . $lines[$i][0] . '. Criteria: ' . $lines[$i][1];
+					}
+				}
+			}
+			catch(Exception $error_message)
+			{
+				echo $error_message;
+			}
+			
+			return array('results' => $results, 'errors' => $errors);
+			}
 	}
 	
 	/**
@@ -126,5 +130,5 @@ class Dummy_data extends CI_Controller {
 }
 
 
-/* End of file dummy_data.php */
-/* Location: ./application/controllers/dummy_data.php */
+// End of file dummy_data.php 
+// Location: ./application/controllers/dummy_data.php
