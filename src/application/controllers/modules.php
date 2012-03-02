3b41d65ae3b80165523d0fcc344539d55a052c00
jmahoney127 <jmahoney@cerds-imac.local>
2012-03-02 15:23:56 +0000

    Prettifying modules.php

--- src/application/controllers/modules.php
+++ src/application/controllers/modules.php
@@ -1,65 +1,78 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	/**
+ 	* MyClass File Doc Comment
+ 	*
+ 	* PHP Version 5
+ 	*
+ 	* @category CourseDataAPIs
+ 	* @package	CourseDataAPIs
+ 	* @author   Jamie Mahoney <jmahoney127@googlemail.com>
+ 	* @license  http://www.php.net/license/3_01.txt  PHP License 3.01
+ 	* @link     coursedata.blogs.lincoln.ac.uk
+ 	*
+ 	*/
+
+	/**
+ 	* Modules
+ 	*
+ 	* @category CourseDataAPIs
+	* @package	CourseDataAPIs
+ 	* @author   Jamie Mahoney <jmahoney127@googlemail.com>
+ 	* @license  http://www.php.net/license/3_01.txt  PHP License 3.01
+ 	* @link     coursedata.blogs.lincoln.ac.uk
+ 	*/
 
 class Modules extends CI_Controller {
 
 	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
+	* Default function for controller.
+	*
+	* @return Nothing
+	* @access Public
+	*/
 	public function index()
 	{
 		$results = array();
 		
-		if(isset($_GET['code']))
-			$this->mongo_db->where('_id', $_GET['code']);
+		if($this->input->get('code'))
+			$this->mongo_db->where('_id', $this->input->get('code'));
 			
-		if(isset($_GET['title']))
-			$this->mongo_db->like('title', $_GET['title']);
+		if($this->input->get('title'))
+			$this->mongo_db->like('title', $this->input->get('title'));
 		
-		if(isset($_GET['department']))
-			$this->mongo_db->like('owningDepartment', $_GET['department']);
+		if($this->input->get('department'))
+			$this->mongo_db->like('owningDepartment', $this->input->get('department'));
 			
-		if(isset($_GET['subject']))
-			$this->mongo_db->like('subject', $_GET['subject']);
+		if($this->input->get('subject'))
+			$this->mongo_db->like('subject', $this->input->get('subject'));
 		
-		if(isset($_GET['synopsis'])){
-			$parameters = explode(',', $_GET['synopsis']);
+		if($this->input->get('synopsis')){
+			$parameters = explode(',', $this->input->get('synopsis'));
 			foreach($parameters as $p)
 				$this->mongo_db->like('moduleSynopsis', $p);
 		}
 		
-		if(isset($_GET['syllabus'])){
-			$parameters = explode(',', $_GET['syllabus']);
+		if($this->input->get('syllabus')){
+			$parameters = explode(',', $this->input->get('syllabus'));
 			foreach($parameters as $p)
 				$this->mongo_db->like('outlineSyllabus', $p);
 		}
 		
-		if(isset($_GET['jacs'])){
-			$parameters = explode(',', $_GET['jacs']);
+		if($this->input->get('jacs')){
+			$parameters = explode(',', $this->input->get('jacs'));
 			foreach($parameters as $p)
 				$this->mongo_db->like('jacsCodes', $p);
 		}
 		
-		if(isset($_GET['outcomes'])){
-			$parameters = explode(',', $_GET['outcomes']);
+		if($this->input->get('outcomes')){
+			$parameters = explode(',', $this->input->get('outcomes'));
 			foreach($parameters as $p)
 				$this->mongo_db->like('learningOutcomes', $p);
 		}
 			
 		$output = $this->mongo_db->get('modules');
 			
-		if($output != null)
+		if($output !== NULL)
 		{
 			$results['error'] = 0;
 			$results['count'] = count($output);
@@ -78,5 +91,5 @@ class Modules extends CI_Controller {
 	
 }
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
+// End of file welcome.php
+// Location: ./application/controllers/welcome.php
